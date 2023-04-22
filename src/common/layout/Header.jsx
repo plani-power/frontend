372ff6d80f0fcb29f76b7f002d0a9f35514764ec
Tomas1876 <mnd3259@gmail.com>
2023-04-23 00:28:10 +0900

    Feat: 스타일드 컴포넌트 추가 후 헤더 작업중

--- src/common/layout/Header.jsx
+++ src/common/layout/Header.jsx
@@ -1,26 +1,46 @@
 import { ROUTES } from "constants/global";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const navMenus = [
     { name: ROUTES.main.name, url: ROUTES.main.url },
-    { name: ROUTES.plans.name, url: ROUTES.plans.url }
-]
+    { name: ROUTES.plans.name, url: ROUTES.plans.url },
+];
 
+const HeaderBar = styled.header`
+    width: 100%;
+    height: 40px;
+    padding: 10px 20px;
+    border-bottom: 1px solid #E5E5E5;
+    button {
+        display: block;
+        margin-left: auto;
+    }
+`
+const Draw = styled.ul`
+    width: 400px;
+    height: 100vh;
+    margin-top: -40px;
+    z-index: 1;
+    position: 
+`
 const Header = () => {
     return (
         <>
-            <header>
-                <span class="material-symbols-outlined">
-                    menu
-                </span>
-            </header>
+            <HeaderBar>
+                <button>
+                    <span className="material-symbols-outlined ">menu</span>
+                </button>
+            </HeaderBar>
             <ul>
-                {
-                    navMenus.map((menu) => <Link to={menu.url} ><li>{menu.name}</li></Link>)
-                }
+                {navMenus.map((menu) => (
+                    <Link to={menu.url}>
+                        <li>{menu.name}</li>
+                    </Link>
+                ))}
             </ul>
         </>
-    )
-}
+    );
+};
 
 export default Header;
--- src/common/layout/Layout.jsx
+++ src/common/layout/Layout.jsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import styled from "styled-components";
+
+const Body = styled.div`
+    width: 100%;
+    max-width: 420px;
+    height: 100vh;
+    background-color: white;
+    margin: 0 auto;
+`
 
 const Layout = () => {
 
     return (
-        <div class="body">
+        <Body>
             <Header />
             <Outlet />
             <Footer />
-        </div>
+        </Body>
     )
 }
 
