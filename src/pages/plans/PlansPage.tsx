f8f8747d4ded3e9b9f014ce488d2bc2447719cfe
hotdog1004 <hotdog87675210@gmail.com>
2023-04-30 17:05:04 +0900

    Feat : 플랜 생성하기 페이지 추가
    플랜 생성하기 페이지 신규 생성, 라우터 파일 수정

--- src/pages/plans/PlansPage.tsx
+++ src/pages/plans/PlansPage.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const PlansPage = () => {
+
     return (
-        <p>
-            플랜 목록 페이지입니다
-        </p>
+        <div>
+            <p>
+                플랜 목록 페이지입니다
+            </p>
+            <button><Link to='/plans/CreatePlan'>플랜 생성하기</Link></button>
+        </div>
     )
 }
 
---
+++ src/pages/plans/createPlan.tsx
@@ -0,0 +1,9 @@
+const createPlan = () => {
+    return (
+        <p>
+            플랜 생성하기 페이지
+        </p>
+    )
+}
+
+export default createPlan;
