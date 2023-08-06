23a8388fe643635f0694170e3bf03d7a518f3773
hotdog1004 <hotdog87675210@gmail.com>
2023-08-06 16:30:56 +0900

    Style : 프리티어 적용

--- src/common/components/SelectBox.tsx
+++ src/common/components/SelectBox.tsx
@@ -1,58 +1,57 @@
-import React, { ChangeEvent } from 'react';
-import styled from 'styled-components';
+import React, { ChangeEvent } from "react";
+import styled from "styled-components";
 
 export interface SelectOption {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 }
 interface Props {
-    value?: string;
-    options: SelectOption[];
-    size?: string
-    onChange: (e:ChangeEvent<HTMLSelectElement>) => void;
-
+  value?: string;
+  options: SelectOption[];
+  size?: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
 const SelectWrapper = styled.div`
-    position: relative;
-`
+  position: relative;
+`;
 const Select = styled.select`
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    -o-appearance: none;
-    -ms-appearance: none;
-    appearance: none;
-    border-radius: 4px;
-    vertical-align: middle;
-    font-size: inherit;
-    color: inherit;
-    box-sizing: content-box;
-    margin: 0;
-    padding: 0.75rem 2.5rem 0.65rem 0.75rem;
-    box-shadow: none;
-    box-sizing: border-box;
-    display: block;
-    width: ${(props)=>props.size || '100%'};
-    border: 1px solid #ddd;
-    line-height: 1.06;
-    cursor: pointer;
-    & ::-ms-expand {
-        display: none;
-    }
-`
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  -o-appearance: none;
+  -ms-appearance: none;
+  appearance: none;
+  border-radius: 4px;
+  vertical-align: middle;
+  font-size: inherit;
+  color: inherit;
+  box-sizing: content-box;
+  margin: 0;
+  padding: 0.75rem 2.5rem 0.65rem 0.75rem;
+  box-shadow: none;
+  box-sizing: border-box;
+  display: block;
+  width: ${(props) => props.size || "100%"};
+  border: 1px solid #ddd;
+  line-height: 1.06;
+  cursor: pointer;
+  & ::-ms-expand {
+    display: none;
+  }
+`;
 
-const SelectBox = ({value, options, onChange, size}:Props) => {
-    return (
-        <SelectWrapper>
-            <Select onChange={onChange}>
-                {options.map(({value, label})=>(
-                    <option key={value} value={value}>
-                        {label}
-                    </option>
-                ))}
-            </Select>
-        </SelectWrapper>
-    )
-}
+const SelectBox = ({ value, options, onChange, size }: Props) => {
+  return (
+    <SelectWrapper>
+      <Select onChange={onChange}>
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </Select>
+    </SelectWrapper>
+  );
+};
 
 export default SelectBox;
